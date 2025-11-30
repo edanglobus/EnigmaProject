3d86b7804e108f5428eca6383bf7c22122c72563
matanegel <mateg1278@gmail.com>
2025-11-30 15:12:03 +0200

    add main and update the branch

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MainManager mainManager = new MainManager();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Choose an order:\n1. Load XML file\n3. Manual Configuration\n5. Encode/Decode String\n0. Exit");
+            String choice = sc.nextLine().trim();
+            try {
+                switch (choice) {
+                    case "1" -> mainManager.order1();
+                    case "3" -> mainManager.order3();
+                    case "5" -> mainManager.order5();
+                    case "0" -> {
+                        System.out.println("Exiting program.");
+                        return;
+                    }
+                    default -> System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    };
+};
